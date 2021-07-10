77ad974ff1164808d052c3317f268a35c2940380
Felipe Garcia Fogal <felipefogal@gmail.com>
2021-07-09 21:19:36 -0300

    Concluindo aula 2 do curso de Apis DotNet Core 5

---
+++ FilmesAPIFogal/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesAPIFogal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPIFogal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme(Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
